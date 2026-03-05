58d643408a990619796b35a32b57a31d812e9ffb
lokeshramchand-ctrl <lokeshramchand@gmail.com>
2026-03-05 10:09:54 +0000

    Implement Binary Search Tree operations: insert and delete

---
+++ code/Tre/operations.java
@@ -0,0 +1,55 @@
+package code.Tre;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+public class operations {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+        if (root.val > val) {
+            root.left = insertIntoBST(root.left, val);
+        } else {
+            root.right = insertIntoBST(root.right, val);
+        }
+        return root;
+    }
+
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val > key) {
+            root.left = deleteNode(root.left, key);
+        } else if (root.val < key) {
+            root.right = deleteNode(root.right, key);
+        } else {
+            if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            } else {
+                TreeNode temp = findMin(root.right);
+                root.val = temp.val;
+                root.right = deleteNode(root.right, temp.val);
+            }
+        }
+        return root;
+    }
+
+    private TreeNode findMin(TreeNode node) {
+        while (node.left != null) {
+            node = node.left;
+        }
+        
+        return node;
+    }
+}
