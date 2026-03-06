36e524cc5ab26d3034289f2b92c38a7e85f29913
lokeshramchand-ctrl <lokeshramchand@gmail.com>
2026-03-06 18:49:17 +0000

    Implement TreeNode class and add traversal methods: inorder, preorder, and postorder

---
+++ code/Tre/operations.java
@@ -0,0 +1,55 @@
+package code.Tre;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+public class operations {
+
+    public TreeNode create() {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        return root;
+    }
+
+    public void preorder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.val + " ");
+        preorder(root.left);
+        preorder(root.right);
+
+    }
+
+    public void postorder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        postorder(root.left);
+        postorder(root.right);
+        System.out.print(root.val + " ");
+
+    }
+    public void inorder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.val + " ");
+        inorder(root.right);
+
+    }
+
+}
