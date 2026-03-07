c109e0492a9a1a5ff3e2e33716d9b7e891bd8251
lokeshramchand-ctrl <lokeshramchand@gmail.com>
2026-03-07 11:53:23 +0000

    Add initial implementation of kth_largest class with TreeNode definition

---
+++ code/Tre/kth_largest.java
@@ -0,0 +1,21 @@
+package code.Tre;
+
+class TreeNode
+{
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val)
+    {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+
+public class kth_largest
+{
+
+}
