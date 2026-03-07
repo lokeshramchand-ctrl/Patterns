f2a8201b8ac3ec9557c7836bffcd53f9fbd380b9
lokeshramchand-ctrl <lokeshramchand@gmail.com>
2026-03-07 19:04:15 +0000

    Add implementation for Diameter of Binary Tree

---
+++ code/SQ/543.diameter-of-binary-tree.java
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=543 lang=java
+ *
+ * [543] Diameter of Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    private int dia = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        depth(root);
+        return dia;
+    }
+
+    private int depth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = depth(root.left);
+        int right = depth(root.right);
+        dia = Math.max(dia, left + right);
+        return Math.max(left, right) + 1;
+    }
+}
+// @lc code=end
