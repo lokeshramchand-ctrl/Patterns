b6cbc7d9cd1d76d867f68b359edb658358d7c7c7
lokeshramchand-ctrl <lokeshramchand@gmail.com>
2026-03-13 18:13:58 +0000

    Refactor twoSum method to use a while loop instead of a for loop for improved clarity

--- code/Array/167.two-sum-ii-input-array-is-sorted.java
+++ code/Array/167.two-sum-ii-input-array-is-sorted.java
@@ -9,7 +9,7 @@ class Solution {
     public int[] twoSum(int[] numbers, int target) {
         int[] res = new int[2];
         int left = 0, right = numbers.length - 1;
-        for (int i = 0; i < numbers.length; i++) {
+        while(left < right) {
             if (numbers[left] + numbers[right] == target) {
                 res[0] = left + 1;
                 res[1] = right + 1;
