19508e3222db8977b3cbd516a054509ba7594b90
lokeshramchand-ctrl <lokeshramchand@gmail.com>
2026-03-14 04:02:42 +0000

    Add implementation for Container With Most Water problem

---
+++ code/Array/11.container-with-most-water.java
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=11 lang=java
+ *
+ * [11] Container With Most Water
+ */
+
+// @lc code=start
+class Solution {
+    public int maxArea(int[] height) {
+        int max = 0;
+        int i = 0;
+        int j = height.length - 1;
+        while (i < j) {
+            int area = (j - i) * Math.min(height[i], height[j]);
+            max = Math.max(max, area);
+            if (height[i] < height[j]) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return max;
+    }
+
+}
+// @lc code=end
