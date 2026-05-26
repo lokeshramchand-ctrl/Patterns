642f369a8c4ad93fa28af6c7d756a0b6a28b6c37
lokeshramchand-ctrl <lokeshramchand@gmail.com>
2026-05-26 18:37:39 +0530

    Refactor twoSum method to improve readability and maintainability

--- code/Array/1.two-sum.java
+++ code/Array/1.two-sum.java
@@ -11,14 +11,13 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            int com = target - nums[i];
-            if (map.containsKey(com)) {
-                return new int[] { map.get(com), i };
-            } else {
-                map.put(nums[i], i);
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
             }
+            map.put(nums[i], i);
         }
-        return new int[] {};
+        throw new IllegalArgumentException("No two sum solution");
     }
 }
 
